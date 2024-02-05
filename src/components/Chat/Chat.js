b7a240c8629488b9e2e01d814be7eb1770c97f53
devhieu <hieudd2090@gmail.com>
2024-02-05 19:23:32 +0700

    online chat complete

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,23 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import ScrollableFeed from "react-scrollable-feed";
 
 import Message from "./Message/Message";
 import styles from "./Chat.module.scss";
 
-export default function Chat() {
+export default function Chat({ socket, roomId, userName }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageData, setMessageData] = useState([]);
+
+  const sendMessage = async () => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: roomId,
+        user: userName,
+        message: currentMessage,
+        time:
+          new Date(Date.now()).getHours() +
+          ":" +
+          new Date(Date.now()).getMinutes(),
+      };
+      await socket.emit("send_message", messageData);
+      setMessageData((list) => [...list, messageData]);
+    }
+  };
+
+  useEffect(() => {
+    socket.off("receive_message").on("receive_message", (data) => {
+      setMessageData((list) => [...list, data]);
+      console.log(messageData);
+    });
+  }, [socket, messageData]);
+
   return (
     <div className={styles.wrapper}>
-      <div className={styles.PlayerChat}>
-        <Message text="hi" player="hieu" />
-        <Message text="hi" player="hieu" />
-        <Message text="hi" player="hieu" />
-        <Message text="hi" player="hieu" />
-        <Message text="hi" player="hieu" />
-        <Message text="hi" player="hieu" />
-        <Message text="hi" player="hieu" />
-        <Message text="hi" player="hieu" />
-        <Message text="hi" player="hieu" />
+      <ScrollableFeed className={styles.PlayerChat}>
+        {messageData.map((data, index) => {
+          return (
+            <div
+              key={index}
+              className={data.user === userName ? styles.user : styles.rival}
+            >
+              <Message text={data.message} player={data.user} />
+            </div>
+          );
+        })}
+      </ScrollableFeed>
+      <div className={styles.chat_footer}>
+        <input
+          type="text"
+          className={styles.chat_box}
+          onChange={(event) => {
+            setCurrentMessage(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            sendMessage();
+          }}
+          className={styles.sendButton}
+        >
+          Send
+        </button>
       </div>
-      <input type="text" className={styles.chat_box} />
     </div>
   );
 }
